2d05a3ef2521182ec3c1ed9d251e6a761c7903b7
Christian Klinger <cklinger@novareto.de>
2011-01-03 13:30:07 +0000

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='grokcore.xmlrpc',
-    version='0.1',
+    version='1.0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
