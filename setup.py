736cda9401585b863369744820e71338d0a798f0
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-10 15:12:51 +0100

    Preparing release 3.0.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='grokcore.xmlrpc',
-    version='1.2.2.dev0',
+    version='3.0.0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
