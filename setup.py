8482e19845378d931f1fb10dda3ca39bcce66645
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-10 15:13:01 +0100

    Back to development: 3.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='grokcore.xmlrpc',
-    version='3.0.0',
+    version='3.0.1.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
