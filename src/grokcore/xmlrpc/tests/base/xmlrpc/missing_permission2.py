fd89a17e8416c0e786af0da047ce9f3445f76c81
Michael Howitz <mh@gocept.com>
2020-12-17 15:57:30 +0100

    Fix tests for Python 2.

--- src/grokcore/xmlrpc/tests/base/xmlrpc/missing_permission2.py
+++ src/grokcore/xmlrpc/tests/base/xmlrpc/missing_permission2.py
@@ -3,7 +3,9 @@ A permission has to be defined first (using grok.Permission for example)
 before it can be used in @grok.require().
 
   >>> from grokcore.xmlrpc import testing
-  >>> testing.grok(__name__)
+
+  # PY2 - remove '+IGNORE_EXCEPTION_DETAIL'  when dropping Python 2 support:
+  >>> testing.grok(__name__)  # doctest: +IGNORE_EXCEPTION_DETAIL
   Traceback (most recent call last):
   ...
   zope.configuration.config.ConfigurationExecutionError: \
